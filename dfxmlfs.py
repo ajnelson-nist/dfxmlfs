c756eca1c43993e9efa412c7dbd919b8dcaf45bd
Alex Nelson <alexander.nelson@nist.gov>
2016-01-06 10:47:26 -0500

    Add NIST software disclaimer text
    Signed-off-by: Alex Nelson <alexander.nelson@nist.gov>
    Reviewed-by: Lee Badger <mark.badger@nist.gov>

--- dfxmlfs.py
+++ dfxmlfs.py
@@ -1,13 +1,25 @@
 #!/usr/bin/env python
 
-#    HelloFS Copyright (C) 2006  Andrew Straw  <strawman@astraw.com>
+# HelloFS Copyright (C) 2006  Andrew Straw  <strawman@astraw.com>
 #
-#    DFXMLFS Copyright (C) 2015  Prometheus Computing, LLC.
-#      Implemented by Alex Nelson <a.nelson@prometheuscomputing.com>
+# DFXMLFS Copyright (C) 2015  Prometheus Computing, LLC.
+#   Implemented by Alex Nelson <a.nelson@prometheuscomputing.com>
 #
-#    This program can be distributed under the terms of the GNU LGPL v2.1.
-#    See the file COPYING.
+# This program can be distributed under the terms of the GNU LGPL v2.1.
+# See the file COPYING.
 #
+# For changes made after January 1, 2016:
+#
+# This software was developed at the National Institute of Standards
+# and Technology by employees of the Federal Government in the course
+# of their official duties. Pursuant to title 17 Section 105 of the
+# United States Code this software is not subject to copyright
+# protection and is in the public domain. NIST assumes no
+# responsibility whatsoever for its use by other parties, and makes
+# no guarantees, expressed or implied, about its quality,
+# reliability, or any other characteristic.
+#
+# We would appreciate acknowledgement if the software is used.
 
 __version__ = "0.0.1"
 
